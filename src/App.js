d86c590fc55f48e4b0e3b9f556939a490c0ca0ec
mtalhamrl <110231439+mtalhamrl@users.noreply.github.com>
2024-09-09 18:49:16 +0300

    react to do app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import ToDoTable from "./components/ToDoTable";
 function App() {
+  const toDos = [
+    { rowNumber: 1, rowDescription: "Feed cat", rowAssigned: "Talha" },
+    { rowNumber: 2, rowDescription: "Gym", rowAssigned: "Genar" },
+    { rowNumber: 3, rowDescription: "Study", rowAssigned: "Meral" },
+    { rowNumber: 4, rowDescription: "Work", rowAssigned: "Talha" },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body">
+          <ToDoTable toDos={toDos} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/ToDoTable.js
@@ -0,0 +1,25 @@
+import ToDoRowItem from "./ToDoRowItem";
+
+function ToDoTable(props) {
+  return (
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Description</th>
+          <th scope="col">Assigned</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.toDos.map((todo) => (
+          <ToDoRowItem
+            rowNumber={todo.rowNumber}
+            rowDescription={todo.rowDescription}
+            rowAssigned={todo.rowAssigned}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+export default ToDoTable;
